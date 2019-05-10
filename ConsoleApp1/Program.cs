f55a5bfb9d669514999343c443277f5a76958995
Rin Nguyen <rinnguyen@kms-technology.com>
2019-05-10 10:23:24 +0700

    add unit test

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -2,11 +2,16 @@
 
 namespace ConsoleApp1
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+        }
+
+        public static int Test()
+        {
+            return 1;
         }
     }
 }
