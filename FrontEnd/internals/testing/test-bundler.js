806dd57733e96fa467c44604a03eaa13f7ed2b92
Rin Nguyen <rinnguyen@kms-technology.com>
2019-05-13 14:50:44 +0700

    unt test

--- FrontEnd/internals/testing/test-bundler.js
+++ FrontEnd/internals/testing/test-bundler.js
@@ -1,3 +1,15 @@
 // needed for regenerator-runtime
 // (ES7 generator support is required by redux-saga)
 import '@babel/polyfill';
+
+/* Add custom settings to Jasmine. */
+/* globals jasmine */
+
+const reporters = require('jasmine-reporters');
+const junitReporter = new reporters.JUnitXmlReporter({
+  consolidateAll: false,
+  savePath: './output/TestResults',
+  filePrefix: 'junit-',
+});
+jasmine.VERBOSE = true;
+jasmine.getEnv().addReporter(junitReporter);
